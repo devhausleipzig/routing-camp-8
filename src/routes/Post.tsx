d80123927978c4a2a7e879fb785fea7703c5cd00
Dan McAtee <danielmcatee@me.com>
2022-12-23 14:07:53 +0100

    add post query

--- src/routes/Post.tsx
+++ src/routes/Post.tsx
@@ -1,6 +1,30 @@
 import { useParams } from "react-router-dom";
+import { PostType } from "./Posts";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+async function getPost(id: string) {
+  const result = await axios.get<PostType>(`http://localhost:3000/posts/${id}`);
+  return result.data;
+}
 
 export function Post() {
   const { postId } = useParams();
-  return <div>Post {postId}</div>;
+  const [post, setPost] = useState<PostType | null>(null);
+
+  useEffect(() => {
+    if (!postId) return;
+    getPost(postId).then((res) => setPost(res));
+  }, []);
+
+  if (!post) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+    </div>
+  );
 }
